0175c81e9900612e9f74d9b2239d467b64d82e63
ismael3440 <162080962+ismael3440@users.noreply.github.com>
2024-05-05 06:33:25 -0400

    Actualizar main.c

--- main.c
+++ main.c
@@ -74,7 +74,7 @@ int main() {
         printf("\n\n(%s***%s) %sSusplantacion ARP iniciada%s\n",
         color(5), color_reset, color(1), color_reset);
 
-        printf("Precione (q/Q) para terminar ataque\n");
+        printf("Use [Q] para terminar ataque\n");
         while (getchar() != ('q' & 'Q'));
 
         send_arp(router_mac, router_ip, target_mac, target_ip, iface);
