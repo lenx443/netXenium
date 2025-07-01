42e88a4c5853c8e97c0d5e894c5b1c469ac25ae1
lenx443 <menasalejandro153@gmail.com>
2025-07-01 18:23:18 -0400

    saved

--- core/interpreter/include/interpreter.h
+++
@@ -1,8 +0,0 @@
-#ifndef __INTERPRETER_H__
-#define __INTERPRETER_H__
-
-#include "parser.h"
-
-void interpreter(Parser_Code *, const char *);
-
-#endif
---
+++ core/interpreter/virtual_machine/include/garbage_collector.h
@@ -0,0 +1,4 @@
+#ifndef __GARBAGE_COLLECTOR_H__
+#define __GARBAGE_COLLECTOR_H__
+
+#endif
