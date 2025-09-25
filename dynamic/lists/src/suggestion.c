368934c42fdda47051e90a2efa58ca22118c2ca8
lenx443 <menasalejandro153@gmail.com>
2025-09-25 01:08:46 -0400

    saved

--- dynamic/lists/src/suggestion.c
+++ dynamic/lists/src/suggestion.c
@@ -1,9 +1,8 @@
-#include <asm-generic/ioctls.h>
-#include <asm-generic/termios.h>
-#include <bits/ioctl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ioctl.h>
+#include <termios.h>
 #include <unistd.h>
 
 #include "colors.h"
