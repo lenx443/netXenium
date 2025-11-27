a24925d6ba30e47e250a7811292c8e785e74d367
lenx443 <menasalejandro153@gmail.com>
2025-11-26 22:25:08 -0500

    saved

--- core/common/instances/method/src/xen_method_implement.c
+++ core/common/instances/method/src/xen_method_implement.c
@@ -1,4 +1,5 @@
 #include "xen_method_implement.h"
+#include "attrs.h"
 #include "basic.h"
 #include "basic_templates.h"
 #include "callable.h"
@@ -9,9 +10,12 @@
 #include "vm_def.h"
 #include "xen_function_instance.h"
 #include "xen_gc.h"
+#include "xen_map.h"
+#include "xen_map_implement.h"
 #include "xen_method_instance.h"
 #include "xen_nil.h"
 #include "xen_string.h"
+#include "xen_vector.h"
 
 static void method_trace(Xen_GCHeader* h) {
   Xen_Method* method = (Xen_Method*)h;
@@ -54,6 +58,73 @@ static Xen_Instance* method_callable(struct __Instance* self,
         method->self, args, kwargs, NULL, function->fun_code);
     if (!run_context_stack_push(&vm->vm_ctx_stack, new_ctx)) {
       return NULL;
+    }
+    Xen_Instance* args_list = Xen_Map_Keys(function->args_names);
+    if (Xen_SIZE(args) > Xen_SIZE(args_list)) {
+      run_context_stack_pop_top(&vm->vm_ctx_stack);
+      return NULL;
+    }
+    for (Xen_size_t i = 0; i < Xen_SIZE(args); i++) {
+      Xen_Instance* name = Xen_Vector_Get_Index(args_list, i);
+      Xen_Instance* arg = Xen_Vector_Get_Index(args, i);
+      if (!name) {
+        run_context_stack_pop_top(&vm->vm_ctx_stack);
+        return NULL;
+      }
+      if (!Xen_Map_Push_Pair(((RunContext_ptr)new_ctx)->ctx_instances,
+                             (Xen_Map_Pair){name, arg})) {
+        run_context_stack_pop_top(&vm->vm_ctx_stack);
+        return NULL;
+      }
+    }
+    if (Xen_IMPL(kwargs) == &Xen_Map_Implement) {
+      Xen_Instance* kwargs_it = Xen_Attr_Iter(kwargs);
+      if (!kwargs_it) {
+        run_context_stack_pop_top(&vm->vm_ctx_stack);
+        return NULL;
+      }
+      Xen_Instance* keyword = NULL;
+      while ((keyword = Xen_Attr_Next(kwargs_it)) != NULL) {
+        if (!Xen_Map_Has(function->args_names, keyword)) {
+          run_context_stack_pop_top(&vm->vm_ctx_stack);
+          return NULL;
+        }
+        if (Xen_Map_Has(((RunContext_ptr)new_ctx)->ctx_instances, keyword)) {
+          run_context_stack_pop_top(&vm->vm_ctx_stack);
+          return NULL;
+        }
+        Xen_Instance* value = Xen_Map_Get(kwargs, keyword);
+        if (!Xen_Map_Push_Pair(((RunContext_ptr)new_ctx)->ctx_instances,
+                               (Xen_Map_Pair){keyword, value})) {
+          run_context_stack_pop_top(&vm->vm_ctx_stack);
+          return NULL;
+        }
+      }
+    }
+    Xen_Instance* defaults_it = Xen_Attr_Iter(function->args_default_values);
+    if (!defaults_it) {
+      run_context_stack_pop_top(&vm->vm_ctx_stack);
+      return NULL;
+    }
+    Xen_Instance* default_name = NULL;
+    while ((default_name = Xen_Attr_Next(defaults_it)) != NULL) {
+      if (!Xen_Map_Has(((RunContext_ptr)new_ctx)->ctx_instances,
+                       default_name)) {
+        Xen_Instance* default_value =
+            Xen_Map_Get(function->args_default_values, default_name);
+        if (!Xen_Map_Push_Pair(((RunContext_ptr)new_ctx)->ctx_instances,
+                               (Xen_Map_Pair){default_name, default_value})) {
+          run_context_stack_pop_top(&vm->vm_ctx_stack);
+          return NULL;
+        }
+      }
+    }
+    for (Xen_ssize_t i = 0; i < function->args_requireds; i++) {
+      Xen_Instance* name = Xen_Vector_Get_Index(args_list, i);
+      if (!Xen_Map_Has(((RunContext_ptr)new_ctx)->ctx_instances, name)) {
+        run_context_stack_pop_top(&vm->vm_ctx_stack);
+        return NULL;
+      }
     }
   } else if (function->fun_type == 2) {
     Xen_Instance* ret = function->fun_native(method->self, args, kwargs);
